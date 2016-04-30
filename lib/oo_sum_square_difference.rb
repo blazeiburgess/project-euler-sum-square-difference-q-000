0f08f46fcbbe0062b465b0f38d316ecef1d7d037
thedailyephemera <thedailyephemera@gmail.com>
2016-04-30 14:24:26 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+	def initialize(limit)
+		@limit = limit
+	end
+	def difference
+		sq_t, sm_t = 0, 0
+		(1..@limit).each do |num|
+			sm_t += num ** 2
+			sq_t += num
+		end
+		return sq_t **2 - sm_t
+	end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,9 @@
 # Implement your procedural solution here!
+def sum_square_difference(limit)
+	sq_t, sm_t = 0, 0
+	(1..limit).each do |num| 
+		sm_t += num ** 2
+		sq_t += num
+	end
+	return sq_t**2 - sm_t
+end
